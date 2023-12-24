d711c6ecff5587298f8c6ddd37ac5e27312cf629
enesucar <enesucar1409@gmail.com>
2023-12-25 02:08:08 +0300

    feat(BusinessProblemDetails): change key name

--- src/Quesify.SharedKernel.AspNetCore/HttpProblemDetails/BusinessProblemDetails.cs
+++ src/Quesify.SharedKernel.AspNetCore/HttpProblemDetails/BusinessProblemDetails.cs
@@ -8,9 +8,9 @@ public class BusinessProblemDetails : ProblemDetails
     {
         Title = "Rule Violation";
         Detail = detail;
-        Status = StatusCodes.Status409Conflict;
+        Status = StatusCodes.Status422UnprocessableEntity;
         Type = "https://datatracker.ietf.org/doc/html/rfc7231#section-6.5.8";
         Instance = instance;
-        if (errors != null) Extensions.Add("Errors", errors);
+        if (errors != null) Extensions.Add("errors", errors);
     }
 }
