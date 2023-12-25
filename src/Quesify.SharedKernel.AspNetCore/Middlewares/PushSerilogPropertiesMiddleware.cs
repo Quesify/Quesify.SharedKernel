69afcb3fa9f84f4611b47a0a63fe92327663d754
enesucar <enesucar1409@gmail.com>
2023-12-25 19:13:14 +0300

    refactor(Serilog): serilog pushproperty UserId change claim type and remove AcceptLanguage, TimeZoneIdentifier, UserAgent log property

--- src/Quesify.SharedKernel.AspNetCore/Middlewares/PushSerilogPropertiesMiddleware.cs
+++ src/Quesify.SharedKernel.AspNetCore/Middlewares/PushSerilogPropertiesMiddleware.cs
@@ -2,6 +2,7 @@
 using Quesify.SharedKernel.AspNetCore.HttpFeatures;
 using Serilog.Context;
 using System.Net;
+using System.Security.Claims;
 
 namespace Quesify.SharedKernel.AspNetCore.Logging;
 
@@ -12,16 +13,16 @@ public class PushSerilogPropertiesMiddleware : IMiddleware
         var httpRequestTimeFeature = context.Features.Get<IHttpRequestTimeFeature>();
 
         LogContext.PushProperty("Host", context.Request.Host.ToString());
-        LogContext.PushProperty("UserId", context.User.Identity?.Name?.ToGuid() ?? null);
+        LogContext.PushProperty("UserId", context.User.Claims.FirstOrDefault(o => o.Type == ClaimTypes.NameIdentifier)?.Value.ToGuid() ?? null);
         LogContext.PushProperty("ClientIPAddress", context.Connection.RemoteIpAddress?.ToString());
         LogContext.PushProperty("ClientName", Dns.GetHostEntry(context.Connection.RemoteIpAddress?.ToString()!).HostName);
         LogContext.PushProperty("CorrelationId", context.TraceIdentifier);
         LogContext.PushProperty("RequestQueryString", context.Request.GetQueryString());
         LogContext.PushProperty("RequestMethod", context.Request.Method);
         LogContext.PushProperty("RequestExecutionTime", httpRequestTimeFeature!.RequestDate);
-        LogContext.PushProperty("AcceptLanguage", context.Request.GetHeader(HeaderConstants.AcceptLanguage));
-        LogContext.PushProperty("TimeZoneIdentifier", context.Request.GetHeader(HeaderConstants.TimeZoneIdentifier));
-        LogContext.PushProperty("UserAgent", context.Request.GetHeader(HeaderConstants.UserAgent));
+        //LogContext.PushProperty("AcceptLanguage", context.Request.GetHeader(HeaderConstants.AcceptLanguage));
+        //LogContext.PushProperty("TimeZoneIdentifier", context.Request.GetHeader(HeaderConstants.TimeZoneIdentifier));
+        //LogContext.PushProperty("UserAgent", context.Request.GetHeader(HeaderConstants.UserAgent));
         LogContext.PushProperty("RequestMethod", context.Request.Method);
 
         await next(context);
